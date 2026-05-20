8e5b9b5407faa18df0451aadc460662188b0e7d3
antonioleonardovi-rgb <antonioleonardovi@gmail.com>
2026-05-19 21:54:56 -0300

    Javascript

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-var x = 1;
-var y = 2;
-var nome = "Leo"
+var x = 7;
+let y = 5;
+const z = 6;
+var nome = `Léo`;
+console.log(nome);
+console.log(y);
+console.log(z);
+console.log(x);
 
-console.log(`O usuário ${nome} tem nota ${x+y}`);
+var soma = x + y;
+var soma2 = z + nome
+
+console.log(soma);
+console.log(z + nome); /* numéro com string é concatenação */
+
